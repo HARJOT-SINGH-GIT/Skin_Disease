8886d1f7e833d489f0ded348e8847ef77e1c3f69
HARJOT-SINGH-GIT <115471525+HARJOT-SINGH-GIT@users.noreply.github.com>
2023-10-30 22:20:05 +0530

    "un"

--- app.py
+++ app.py
@@ -96,7 +96,7 @@ def predict():
         else: 
             return "Unable to read the file"
 if __name__ == "__main__":
-    app.run(debug=True,host="0.0.0.0",port=process.env.PORT)
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
     
             
             
